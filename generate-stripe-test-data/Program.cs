ef72aa570402703a2ed6fc47995cdef6ab8972a7
Isaac Agyei <agyeis@hotmail.com>
2025-11-19 19:21:04 -0700

    Initial commit

---
+++ generate-stripe-test-data/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// StripeClient pattern (Recommended)
+using Bogus;
+using Stripe;
+
+StripeConfiguration.ApiKey = "REPLACE_WITH_ENV_VAR";
+
+var testOrders = new Faker<CustomerCreateOptions>()
+    .RuleFor(o => o.Name, f => f.Name.FullName())
+    .RuleFor(o => o.Email, f => f.Internet.Email())
+    .Generate(1);
+var options = new CustomerCreateOptions
+{
+  Name = testOrders[0].Name,
+  Email = testOrders[0].Email,
+};
+var service = new CustomerService();
+Customer customer = service.Create(options);
+
+Console.WriteLine($"Created customer {customer.Id} for {customer.Name}");
